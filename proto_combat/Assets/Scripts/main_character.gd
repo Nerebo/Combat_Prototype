2e6594ba167e039878eaa7a58e5b0d4e0c50db3b
Nerebo <andregoesf@gmail.com>
2024-09-13 21:30:27 -0300

    Dia 3 - Combate aperfeiçoado, Animações prontas

--- proto_combat/Assets/Scripts/main_character.gd
+++ proto_combat/Assets/Scripts/main_character.gd
@@ -7,12 +7,13 @@ extends CharacterBody2D
 
 @onready var animation = $Animacao
 var is_attacking = false
-
-var flip_h = false
+var is_dead = false
+var bow_cooldown = true
+var arrow = preload("res://Assets/Scripts/arrow.tscn")
 
 func _physics_process(delta):
 	move()
-	
+
 func _process(delta: float):
 	var right = Input.is_action_pressed("move_right")
 	var left = Input.is_action_pressed("move_left")
@@ -20,42 +21,52 @@ func _process(delta: float):
 	var down = Input.is_action_pressed("move_down")
 	var attack = Input.is_action_pressed("mouse")
 	
-	if(is_attacking == false):
-		if right:
-			if(flip_h == true):
-				scale.x = -1
-				flip_h = false
-			animation.play("walk")
-			return
-		elif left:
-			if(flip_h == false):
-				scale.x = -1
-				flip_h = true
-			animation.play("walk")
-			return
-		elif up:
-			animation.play("walk")
-		elif down:
-			animation.play("walk")
-		elif attack:
-			animation.play("attack")
-			is_attacking = true
-			$Attack_Timer.start()
+	var mouse = get_global_mouse_position()
+	$Marker2D.look_at(mouse)
+	
+	if(is_dead == false):
+		if(is_attacking == false):
+			if right:
+				$Soldier.flip_h = false
+				animation.play("walk")
+				return
+			elif left:
+				$Soldier.flip_h = true
+				animation.play("walk")
+				return
+			elif up:
+				animation.play("walk")
+			elif down:
+				animation.play("walk")
+			elif attack:
+				animation.play("attack")
+				is_attacking = true
+				$Attack_Timer.start()
+			else:
+				animation.play("idle")
+				return
 		else:
-			animation.play("idle")
-			return
-	else:
-		set_physics_process(false)
-		
+			set_physics_process(false)
+	
 func move():
 	var direcao: Vector2 = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
 	
 	velocity = direcao.normalized() * move_speed
 	move_and_slide()
 
+func _die():
+	$Death_Timer.start()
+	animation.play("death")
+	set_physics_process(false)
 
 func _on_attack_timer_timeout() -> void:
+	var arrow_instance = arrow.instantiate()
+	arrow_instance.rotation = $Marker2D.rotation
+	arrow_instance.global_position = $Marker2D.global_position
+	add_child(arrow_instance)
 	is_attacking = false
 	set_physics_process(true)
 
-	
+
+func _on_death_timer_timeout() -> void:
+	pass
--- proto_combat/Assets/Scripts/slime.gd
+++ proto_combat/Assets/Scripts/slime.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 var player_ref = null
+var is_dead: bool = false
+var health: int = 2
 @onready var animation = $AnimationPlayer
 
 @export_category("Objects")
@@ -11,22 +13,42 @@ func _on_detection_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Personagem"):
 		player_ref = body
 
-
-func _on_detection_body_exited(body: Node2D) -> void:
-	if body.is_in_group("Personagem"):
-		player_ref = null
-		
 func _physics_process(delta: float) -> void:
+	if is_dead:
+		return
 	if player_ref != null:
+		if player_ref.is_dead:
+			velocity = Vector2.ZERO
+			move_and_slide()
+			return
+			
 		var direction: Vector2 = global_position.direction_to(player_ref.global_position)
+		var distance: float = global_position.distance_to(player_ref.global_position)
 		velocity = direction * 40
-	
+		
+		if velocity.x > 0:
+			$Sprite2D.flip_h = false
+		elif velocity.x < 0:
+			$Sprite2D.flip_h = true
+		
+		if distance < 27:
+			player_ref.is_dead = true
+			player_ref._die()
+				
 	if player_ref == null:
 		animation.play("idle")
 	elif player_ref != null:
-		if velocity.x > 0:
-			_texture.flip_h = false
-		elif velocity.x < 0:
-			_texture.flip_h = true
 		animation.play("walk")
 	move_and_slide()
+
+func update_health(damage):
+	print("health: ", health)
+	print("damage: ", damage)
+	health = health - damage
+	if(health == 0):
+		is_dead = true
+		animation.play("death")
+
+
+func _on_animation_player_animation_finished(anim_name: String) -> void:
+	queue_free()
